fcc38454b02a50268fb92dc1ed108abd77680f36
Jatin1434Walia <jkw1434@gmail.com>
2023-12-22 23:29:28 +0530

    Video Animation submenu dropdown

---
+++ frontend/src/components/submenu/ProgrammingTech.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProgrammingTech = () => {
+    return (
+        <div className='flex w-full'>
+            <div className="menu_link1 flex flex-col gap-4 w-1/4">
+                <div className="menu_initial_link1 flex flex-col gap-1">
+                    <h1 className='font-medium text-black'>Website Development</h1>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProgrammingTech
