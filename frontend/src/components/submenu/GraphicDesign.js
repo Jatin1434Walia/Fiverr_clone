34bca33158a4664f86dfc6ff872d83ea169c9fe2
Jatin1434Walia <jkw1434@gmail.com>
2023-12-22 22:55:03 +0530

    implemented some changes into navbar for graphic design menu

---
+++ frontend/src/components/submenu/GraphicDesign.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const GraphicDesign = () => {
+  return (
+    <div className='flex w-full'>
+      <div className="menu_link1 flex flex-col gap-4 w-1/4">
+        <div className="menu_initial_link1 flex flex-col gap-1">
+          <h1 className='font-medium text-black'>Logo Brand Identity</h1>
+          <p>Logo Design</p>
+          <p>Brand Style Guides</p>
+          <p>Business Cards & Stationery</p>
+          <p>Fonts & Typography</p>
+          <p>Logo Maker Tool</p>
+        </div>
+        <div className="menu_initial_link2 flex flex-col gap-1">
+          <h1 className='font-medium text-black'>Art & Illustration</h1>
+          <p>Illustration</p>
+          <p>AI Artists</p>
+          <p>Children Book Illustrations</p>
+          <p>Cartoon & Comics</p>
+          <p>Tattoo Design</p>
+          <p>StoryBooks</p>
+        </div>
+        <div className="menu_initial_link3 flex flex-col gap-1">
+          <h1 className='font-medium text-black'>Miscllaneous</h1>
+          <p>Design Advice</p>
+        </div>
+      </div>
+
+      <div className="menu_link2 flex flex-col gap-4 w-1/4">
+        <div className="menu_initial_link4 flex flex-col gap-1">
+          <h1 className='font-medium text-black'>Web & App Design</h1>
+          <p>Website Design</p>
+          <p>App Design</p>
+          <p>UX Design</p>
+          <p>Landpage Design</p>
+          <p>Icon Design</p>
+        </div>
+        <div className="menu_initial_link5 flex flex-col gap-1">
+          <h1 className='font-medium text-black'>Product & Gaming</h1>
+          <p>Industrial & Product Design</p>
+          <p>Character Modelling</p>
+          <p>Game Art</p>
+          <p>Graphic For Streamers</p>
+        </div>
+        <div className="menu_initial_link6 flex flex-col gap-1">
+          <h1 className='font-medium text-black'>Print Design</h1>
+          <p>Flyer Design</p>
+          <p>Brochure Design</p>
+          <p>Poster Design</p>
+          <p>Catalogue Design</p>
+        </div>
+      </div>
+
+      <div className="menu_link2 flex flex-col gap-4 w-1/4">
+        <div className="menu_initial_link7 flex flex-col gap-2">
+          <h1 className='font-medium text-black'>Visual Design</h1>
+          <p>Image Editing</p>
+          <p>Presentation Design</p>
+          <p>Vector Tracing</p>
+          <p>Resume Design</p>
+        </div>
+        <div className="menu_initial_link8 flex flex-col gap-2">
+          <h1 className='font-medium text-black'>Marketing Design</h1>
+          <p>Social Media Design</p>
+          <p>Email Design</p>
+          <p>Web Banners</p>
+          <p>Signage Design</p>
+        </div>
+        <div className="menu_initial_link9 flex flex-col gap-2">
+          <h1 className='font-medium text-black'>Packaging & covers</h1>
+          <p>Packaging & Labeeling Design </p>
+          <p>Book Design</p>
+          <p>Book Covers</p>
+          <p>Album Cover Design</p>
+        </div>
+      </div>
+      <div className="menu_link2 flex flex-col gap-4 w-1/4">
+        <div className="menu_initial_link10 flex flex-col gap-2">
+          <h1 className='font-medium text-black'>Architectural & Building Design</h1>
+          <p>Architecture & Interior Design</p>
+          <p>Landscape Design</p>
+          <p>Building Engineering</p>
+        </div>
+        <div className="menu_initial_link11 flex flex-col gap-2">
+          <h1 className='font-medium text-black'>Fashion & Merchandise</h1>
+          <p>T-shirts & Merchandise</p>
+          <p>Fashio Design</p>
+          <p>Jwellery Design</p>
+        </div>
+        <div className="menu_initial_link9 flex flex-col gap-2">
+          <h1 className='font-medium text-black'>3D Design</h1>
+          <p>3D Architecture</p>
+          <p>3D Industrial Design</p>
+          <p>3D Fashio & Garments</p>
+          <p>3D Printing Characters</p>
+          <p>3D Landscape</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GraphicDesign
